08c8c099337c8dc885e673413f875c31c4d58226
luke0han <lukeohanlon@outlook.com>
2023-04-06 19:05:49 +0100

    initial commit

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ modalClose = () => {
     .modal('hide')
   ;
 }
-
-
 window.addEventListener('load', () => {
-    
     items = JSON.parse(localStorage.getItem('items')) || [];
     const newItem = document.querySelector('#form-list')
     displayitems()
@@ -25,12 +22,9 @@ window.addEventListener('load', () => {
                 done: false,
                 createdAr: new Date().getTime()
             }
-       
-       
         items.push(item)
         localStorage.setItem('items', JSON.stringify(items))
         e.target.reset()
-       
         displayitems()
         modalClose()
         }
@@ -44,32 +38,15 @@ function displayitems() {
     items.forEach(item => {
         const itemItem = document.createElement('li')
         itemItem.classList.add('item-item')
-
-        const label = document.createElement('label')
         const input = document.createElement('input')
-        const span = document.createElement('span')
         const content = document.createElement('div')
         const actions = document.createElement('div')
-        const edit = document.createElement('button')
         const delBtn = document.createElement('button')
-
-        input.type = 'checkbox'
-        input.checked = item.done
-        span.classList.add('bubble')
-
         content.classList.add('item-content')
         actions.classList.add('actions')
-        // edit.classList.add('edit')
         delBtn.classList.add("delete")
-
         content.innerHTML = item.content
-        // edit.innerHTML = "Edit"
         delBtn.innerHTML = "Delete"
-
-
-        // label.appendChild(input)
-        // label.appendChild(span)
-        // actions.appendChild(edit)
         actions.appendChild(delBtn)
         itemItem.appendChild(content)
         itemItem.appendChild(actions)
@@ -89,9 +66,8 @@ function displayitems() {
             }
             displayitems()
         })
-
         delBtn.addEventListener('click', e => {
-            items = items.filter(t => t != item)
+            items = items.filter(i => i != item)
             localStorage.setItem('items', JSON.stringify(items))
             displayitems()
         })
